d648f3ea3f845360c8fe1ca407d8e336e1e49ddb
Tristan Allaire <tristan.allaire@alarch.fr>
2024-02-27 10:40:31 +0100

    BCD Updated for Atlasnet

--- src/main.js
+++ src/main.js
@@ -149,9 +149,8 @@ let tokenMetadata = new TokenMetadataApi(process.env.VUE_APP_TOKEN_METADATA_API)
 let metadataService = new MetadataAPI(process.env.VUE_APP_METADATA_API_URI);
 let stats = new StatsAPI({
   'mainnet': process.env.VUE_APP_MAINNET_STATS_API_URI,
-  'ghostnet': process.env.VUE_APP_GHOSTNET_STATS_API_URI,
-  'nairobinet': process.env.VUE_APP_TESTNET_STATS_API_URI,
-  'oxfordnet': process.env.VUE_APP_OXFORDNET_STATS_API_URI,
+  'basenet': process.env.VUE_APP_BASENET_STATS_API_URI,
+  'atlasnet': process.env.VUE_APP_ATLASNET_STATS_API_URI,
 });
 
 const darkLocalStorage = localStorage.getItem('dark');
